1a42bae4b4657a772288fa2b7c7088bc1243f85f
Jan <>
2017-11-28 21:38:45 +0100

    Des alte neue; Ohne Sessions

--- src/main/java/de/qreator/vertx/VertxWebFormular.java
+++ src/main/java/de/qreator/vertx/VertxWebFormular.java
@@ -13,25 +13,19 @@ import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
-import java.net.CookieHandler;
 
 public class VertxWebFormular {
-
+    
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
 
         HttpServer server = vertx.createHttpServer();
 
         Router router = Router.router(vertx);
+        
 
-        router.route().handler(CookieHandler.create());
-
-        SessionStore baum = LocalSessionStore.create(vertx);
-
-        SessionHandler praktikant = SessionHandler.create(baum);
-
-        router.route().handler(praktikant);
-
+        
+        
         
         router.route("/anfrage").handler(routingContext -> {
             String typ = routingContext.request().getParam("typ");
@@ -52,9 +46,7 @@ public class VertxWebFormular {
                 jo.put("typ", "antwort");
                 if (name1.equals(Passwort)&& name.equals(user)){
                 jo.put("text", "Der Name war " + name +" und das passwort ist gültig "+ name1);
-               Session session = routingContext.session();
                 jo.put("angemeldet",true);
-               
                 
                 
                 
@@ -64,25 +56,6 @@ public class VertxWebFormular {
                }
             }
             response.end(Json.encodePrettily(jo));
-        });
-                
-        router.route("/session").handler(routingContext -> {
-            String typ = routingContext.request().getParam("typ");
-            HttpServerResponse response = routingContext.response();
-            response.putHeader("content-type", "application/json");
-            JsonObject jo = new JsonObject();
-            if (typ.equals("anfrage")){
-                Session session = routingContext.session();
-                jo.put("typ", "angemeldet");
-                if (session.get("angemeldet")==null){
-                    
-                    jo.put("angemeldet","nein");
-                } else if (session.get("angemeldet").equals("ja")){
-                    
-                    jo.put("angemeldet","ja");
-                }
-                response.end(Json.encodePrettily(jo));
-            }
         });
 
         // statische html-Dateien werden über den Dateipfad static ausgeliefert
