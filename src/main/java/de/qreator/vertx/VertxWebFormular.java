5591c66c4b3a21d1a0a85264c306b8f8def1295b
Höllenmaschine 2.0 <Hllenmaschine 2.0@GamingPC.fritz.box>
2017-11-28 14:01:00 +0100

    Session Versuch 1.0

--- src/main/java/de/qreator/vertx/VertxWebFormular.java
+++ src/main/java/de/qreator/vertx/VertxWebFormular.java
@@ -13,6 +13,7 @@ import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import java.net.CookieHandler;
 
 public class VertxWebFormular {
 
@@ -22,7 +23,14 @@ public class VertxWebFormular {
         HttpServer server = vertx.createHttpServer();
 
         Router router = Router.router(vertx);
-        
+
+        router.route().handler(CookieHandler.create());
+
+        SessionStore baum = LocalSessionStore.create(vertx);
+
+        SessionHandler praktikant = SessionHandler.create(baum);
+
+        router.route().handler(praktikant);
 
         
         router.route("/anfrage").handler(routingContext -> {
@@ -44,7 +52,9 @@ public class VertxWebFormular {
                 jo.put("typ", "antwort");
                 if (name1.equals(Passwort)&& name.equals(user)){
                 jo.put("text", "Der Name war " + name +" und das passwort ist gültig "+ name1);
+               Session session = routingContext.session();
                 jo.put("angemeldet",true);
+               
                 
                 
                 
@@ -54,6 +64,25 @@ public class VertxWebFormular {
                }
             }
             response.end(Json.encodePrettily(jo));
+        });
+                
+        router.route("/session").handler(routingContext -> {
+            String typ = routingContext.request().getParam("typ");
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "application/json");
+            JsonObject jo = new JsonObject();
+            if (typ.equals("anfrage")){
+                Session session = routingContext.session();
+                jo.put("typ", "angemeldet");
+                if (session.get("angemeldet")==null){
+                    
+                    jo.put("angemeldet","nein");
+                } else if (session.get("angemeldet").equals("ja")){
+                    
+                    jo.put("angemeldet","ja");
+                }
+                response.end(Json.encodePrettily(jo));
+            }
         });
 
         // statische html-Dateien werden über den Dateipfad static ausgeliefert
